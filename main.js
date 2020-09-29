3477472b1e4d7575094af31e9fd302fae9102f34
haaaru22 <haaaru2200@gmail.com>
2020-09-29 18:47:11 +0900

    完成

--- main.js
+++ main.js
@@ -1,95 +1,106 @@
 console.clear();
 
 function pageTransition() {
-    let tl = gsap.timeline();
-    gsap.to("img", { duration: 0.4, opacity: 0 });
+  let tl = gsap.timeline({ ease: Expo.easeInOut });
 
-    tl.set(".transition-container span", { pointerEvents: "all" })
-        .to("span#from-top", {
-            duration: 0.4,
-            transformOrigin: "top center",
-            scaleY: 1,
-            top: "0 %",
-            delay: 0.2,
-        })
-        .to(
-            "span#from-bottom",
-            {
-                duration: 0.4,
-                transformOrigin: "bottom center",
-                scaleY: 1,
-                delay: 0.2,
-            },
-            "-=0.6"
-        );
-    tl.to("span#from-top", {
+  gsap.to("img", {
+    duration: 0.4,
+    opacity: 0,
+  });
+
+  tl.set(".transition-container span", { pointerEvents: "all" })
+    .to("span#from-top", {
+      duration: 0.4,
+      transformOrigin: "top center",
+      scaleY: 1,
+      top: "0%",
+      delay: 0.2,
+    })
+    .to(
+      "span#from-bottom",
+      {
         duration: 0.4,
         transformOrigin: "bottom center",
+        scaleY: 1,
+        delay: 0.2,
+      },
+      "-=0.6"
+    );
+
+  tl.to("span#from-top", {
+    duration: 0.4,
+    transformOrigin: "bottom center",
+    scaleY: 0,
+    delay: 0.6,
+  })
+    .to(
+      "span#from-bottom",
+      {
+        duration: 0.4,
+        transformOrigin: "top center",
         scaleY: 0,
         delay: 0.6,
-    })
-        .to(
-            "span#from-bottom",
-            {
-                duration: 0.4,
-                transformOrigin: "top center",
-                scaleY: 0,
-                delay: 0.6,
-            },
-            "-=1"
-        )
-        .set(".transition-container", { pointerEvents: "none" });
+      },
+      "-=1"
+    )
+
+    .set(".transition-container span", { pointerEvents: "none" });
 }
 
 function fadeInContent() {
-    let tl = gsap.timeline();
+  let tl = gsap.timeline({ ease: Expo.easeInOut });
+  tl.set(".transition-element", {
+    top: "5%",
+    opacity: 0,
+  })
+    .set("img", { duration: 0.4, opacity: 0 })
 
-    tl.set(".transition-element", { top: "5%", opacity: 0 })
-        .set("img", { opacity: 0 })
-
-        .to(".transition-element", {
-            duration: 0.4,
-            top: "0%",
-            opacity: 1,
-            stagger: 0.1,
-        })
-
-        .to(
-            "img",
-            {
-                duration: 0.4,
-                opacity: 0.45,
-            },
-            "-=0.4"
-        );
+    .to(".transition-element", {
+      duration: 0.4,
+      top: "0%",
+      opacity: 1,
+      stagger: 0.1,
+    })
+    .to("img", { duration: 0.4, opacity: 0.45 }, "-=0.4");
 }
 
 function fadeOutContent() {
-    let tl = gsap.timeline();
-
-    tl.to(".transition-element", {
-        duration: 0.4,
-        top: "5%",
-        opacity: 0,
-        stagger: -0.1,
-    }).to("img", { duration: 0.4, opacity: 0 }, "-=0.4");
+  let tl = gsap.timeline({ ease: Expo.easeInOut });
+  tl.to(".transition-element", {
+    duration: 0.4,
+    top: "5%",
+    opacity: 0,
+    stagger: -0.1,
+  }).to("img", { duration: 0.4, opacity: 0 }, "-=0.4");
 }
 
-
 barba.init({
-    sync: true,
-    transitions: [{
-        async leave() {
-            
-        }
-    }]
-})
+  sync: true,
+
+  transitions: [
+    {
+      async leave() {
+        const done = this.async();
+        pageTransition();
+        fadeOutContent();
+        await delay(1200);
+        done();
+      },
+      async enter() {
+        fadeInContent();
+      },
+      async once() {
+        fadeInContent();
+      },
+    },
+  ],
+});
 
 function delay(n) {
-    n = n || 2000;
-    return new Promise({ done } => {
-        setTimeout(() => {
-            done();
-        }, n);
-});
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
 }
